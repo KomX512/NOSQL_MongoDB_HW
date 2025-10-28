6552d674b7273eee614cf0623c4d01fafd54ee81
KomX <komx512@outlook.com>
2025-10-29 01:01:09 +0500

    first commit

---
+++ src/main/java/NOSQL_MongoDB_HW/NosqlMongoDbHwApplication.java
@@ -0,0 +1,13 @@
+package NOSQL_MongoDB_HW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosqlMongoDbHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NosqlMongoDbHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NOSQL_MongoDB_HW/NosqlMongoDbHwApplicationTests.java
@@ -0,0 +1,13 @@
+package NOSQL_MongoDB_HW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NosqlMongoDbHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
